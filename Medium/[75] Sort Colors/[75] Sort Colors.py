a76c793e3813fbc2e3940bdeee2e9a4976096401
samessi <jsamarth84@gmail.com>
2023-10-07 19:45:36 +0530

    Leetcode-Solutions

---
+++ Medium/[75] Sort Colors/[75] Sort Colors.py
@@ -0,0 +1,18 @@
+"""
+Accepted
+75 [Medium]
+Runtime: 49 ms, faster than 8.99% of Java online submissions for Sort Colors.
+Memory Usage:  16.32 MB, less than 21.85% of Java online submissions for Sort Colors.
+"""
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        for i in range(0,len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if nums[j] < nums[i]:
+                    nums[i] += nums[j]
+                    nums[j] = nums[i] - nums[j]
+                    nums[i] = nums[i] - nums[j]
