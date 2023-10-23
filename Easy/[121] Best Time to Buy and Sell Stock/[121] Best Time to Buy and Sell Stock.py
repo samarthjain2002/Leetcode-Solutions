38b81f7b45e16e1c4014b7c649e452dcc00c3f4b
samessi <jsamarth84@gmail.com>
2023-10-23 18:47:39 +0530

    Leetcode-Solutions

---
+++ Easy/[121] Best Time to Buy and Sell Stock/[121] Best Time to Buy and Sell Stock.py
@@ -0,0 +1,20 @@
+"""
+Accepted
+121 [Easy]
+Runtime: 863 ms, faster than 56.38% of Java online submissions for Best Time to Buy and Sell Stock.
+Memory Usage: 27.18 MB, less than 93.66% of Java online submissions for Best Time to Buy and Sell Stock.
+"""
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left, right = 0, 1
+        maxProfit = 0
+
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                profit = prices[right] - prices[left]
+                maxProfit = max(maxProfit, profit)
+            else:
+                left = right
+            right += 1
+
+        return maxProfit
