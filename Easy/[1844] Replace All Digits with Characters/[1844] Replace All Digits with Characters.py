d07325a6e79def66694f0e019d5ba9cc0f07dc77
samessi <jsamarth84@gmail.com>
2023-11-02 17:55:09 +0530

    Leetcode Solutions

---
+++ Easy/[1844] Replace All Digits with Characters/[1844] Replace All Digits with Characters.py
@@ -0,0 +1,16 @@
+"""
+Accepted
+1844 [Easy]
+Runtime: 41 ms, faster than 40.32% of Java online submissions for Replace All Digits with Characters.
+Memory Usage: 16.2 MB, less than 70.79% of Java online submissions for Replace All Digits with Characters.
+"""
+class Solution:
+    def replaceDigits(self, s: str) -> str:
+        newS = ""
+        for i in range(0,len(s)):
+            if i%2 != 0:
+                newS += chr(ord(s[i-1]) + int(s[i]))
+            else:
+                newS += s[i]
+
+        return newS
