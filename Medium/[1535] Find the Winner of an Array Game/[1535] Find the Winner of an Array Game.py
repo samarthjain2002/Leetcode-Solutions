3e8cdc361847394cc80d5fe15cf2056a3f452aa6
samessi <jsamarth84@gmail.com>
2023-11-05 23:29:10 +0530

    Leetcode-Solutions

---
+++ Medium/[1535] Find the Winner of an Array Game/[1535] Find the Winner of an Array Game.py
@@ -0,0 +1,28 @@
+"""
+Accepted
+1535 [Medium]
+Runtime: 533 ms, faster than 58.70% of Java online submissions for Find the Winner of an Array Game.
+Memory Usage: 29.89 MB, less than 67.39% of Java online submissions for Find the Winner of an Array Game.
+"""
+class Solution:
+    def getWinner(self, arr: List[int], k: int) -> int:
+        left = 0
+        right = 1
+        
+        winner = arr[0]
+        win_count = 0
+        maximum = max(arr)
+
+        while win_count != k:
+            if arr[left] == maximum:
+                return winner
+            if arr[left] > arr[right]:
+                win_count += 1
+                right += 1
+            else:
+                win_count = 1
+                winner = arr[right]
+                left = right
+                right += 1
+
+        return winner
