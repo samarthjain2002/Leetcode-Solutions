1d1ea06658f3d998da2a27f42f46006f0086dd58
samessi <jsamarth84@gmail.com>
2023-11-19 22:11:19 +0530

    Leetcode-Solutions

--- Medium/[53] Maximum Subarray/[53] Maximum Subarray (Python3).py
+++ Medium/[53] Maximum Subarray/[53] Maximum Subarray (Python3).py
@@ -1,8 +1,8 @@
 """
 Accepted
 53 [Medium]
-Runtime: 622 ms, faster than 36.16% of Java online submissions for Maximum Subarray.
-Memory Usage: 29.13 MB, less than 92.01% of Java online submissions for Maximum Subarray.
+Runtime: 622 ms, faster than 36.16% of Python3 online submissions for Maximum Subarray.
+Memory Usage: 29.13 MB, less than 92.01% of Python3 online submissions for Maximum Subarray.
 """
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
