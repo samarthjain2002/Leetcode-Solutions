e17062fadd7b5a17db3a5153778fa026bb92d2cb
samessi <jsamarth84@gmail.com>
2023-11-22 22:16:13 +0530

    Leetcode-Solutions

--- Medium/[75] Sort Colors/[75] Sort Colors.py
+++ Medium/[75] Sort Colors/[75] Sort Colors.py
@@ -2,7 +2,7 @@
 Accepted
 75 [Medium]
 Runtime: 49 ms, faster than 8.99% of Python3 online submissions for Sort Colors.
-Memory Usage:  16.32 MB, less than 21.85% of Python3 online submissions for Sort Colors.
+Memory Usage: 16.32 MB, less than 21.85% of Python3 online submissions for Sort Colors.
 """
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
