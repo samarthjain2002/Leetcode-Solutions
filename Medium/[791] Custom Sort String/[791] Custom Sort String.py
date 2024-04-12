1132e303140725094df909c36a8be6885ea4ef70
samessi <jsamarth84@gmail.com>
2024-04-12 10:34:53 +0530

    Leetcode-Solutions

---
+++ Medium/[791] Custom Sort String/[791] Custom Sort String.py
@@ -0,0 +1,21 @@
+"""
+Accepted
+791 [Medium]
+Runtime: 34 ms, faster than 68.03% of Python3 online submissions for Custom Sort String.
+Memory Usage:  16.48 MB, less than 91.57% of Python3 online submissions for Custom Sort String.
+"""
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        freqDict = defaultdict(int)
+        for char in s:
+            freqDict[char] += 1
+
+        s = ""
+        for char in order:
+            s += char * freqDict[char]
+            freqDict[char] = 0
+
+        for key, val in freqDict.items():
+            s += key * val
+
+        return s
