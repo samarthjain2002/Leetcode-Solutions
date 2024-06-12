d542d1c81981cd37053f76bdc3c1e5f4c940c4e6
samessi <jsamarth84@gmail.com>
2024-06-12 12:12:28 +0530

    Leetcode-Solutions

--- Medium/[75] Sort Colors/[75] Sort Colors.py
+++ Medium/[75] Sort Colors/[75] Sort Colors.py
@@ -1,6 +1,36 @@
 """
 Accepted
 75 [Medium]
+Runtime: 35 ms, faster than 73.49% of Python3 online submissions for Sort Colors.
+Memory Usage: 16.40 MB, less than 79.02% of Python3 online submissions for Sort Colors.
+"""
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zeros, ones = 0, 0
+        for num in nums:
+            if num == 0:
+                zeros += 1
+            elif num == 1:
+                ones += 1
+        for i in range(len(nums)):
+            if zeros:
+                nums[i] = 0
+                zeros -= 1
+            elif ones:
+                nums[i] = 1
+                ones -= 1
+            else:
+                nums[i] = 2
+
+
+
+
+"""
+Accepted
+75 [Medium]
 Runtime: 49 ms, faster than 8.99% of Python3 online submissions for Sort Colors.
 Memory Usage: 16.32 MB, less than 21.85% of Python3 online submissions for Sort Colors.
 """
