6a0c8045e3057d0b65068a462dcaa4ff9f1ba2ef
samessi <jsamarth84@gmail.com>
2024-06-20 20:06:28 +0530

    Leetcode-Solutions

--- Medium/[848] Shifting Letters/[848] Shifting Letters.py
+++ Medium/[848] Shifting Letters/[848] Shifting Letters.py
@@ -1,6 +1,20 @@
 """
 Accepted
 848 [Medium]
-Runtime: 160 ms, faster than 67.72% of Python3 online submissions for Shifting Letters.
-Memory Usage:  18.45 MB, less than 50.76% of Python3 online submissions for Shifting Letters.
+Runtime: 601 ms, faster than 31.25% of Python3 online submissions for Shifting Letters.
+Memory Usage:  30.29 MB, less than 28.92% of Python3 online submissions for Shifting Letters.
 """
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[int]) -> str:
+        prefixSum = sum(shifts)
+        res = ""
+        for i in range(len(s)):
+            temp = prefixSum
+            temp = temp % 26
+            if ord(s[i]) - ord('a') + temp > 25:
+                temp = temp - 26 + ord(s[i]) - ord('a')
+                res += chr(ord('a') + temp)
+            else:
+                res += chr(ord(s[i]) + temp)
+            prefixSum -= shifts[i]
+        return res
