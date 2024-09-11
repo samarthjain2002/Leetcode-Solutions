90fc3c12e43aec84e907b9d1ca8b930e11fe2834
Samarth Jain <jsamarth84@gmail.com>
2024-09-11 10:06:34 +0530

    Leetcode-Solutions

--- Medium/[2807] Insert Greatest Common Divisors in Linked List/[2807] Insert Greatest Common Divisors in Linked List.py
+++ Medium/[2807] Insert Greatest Common Divisors in Linked List/[2807] Insert Greatest Common Divisors in Linked List.py
@@ -1,7 +1,27 @@
-[2807] Insert Greatest Common Divisors in Linked List
 """
 Accepted
-2785 [Medium]
-Runtime: 264 ms, faster than 20.50% of Python3 online submissions for Sort Vowels in a String.
-Memory Usage: 18.86 MB, less than 80.37% of Python3 online submissions for Sort Vowels in a String.
+2807 [Medium]
+Runtime: 62 ms, faster than 88.69% of Python3 online submissions for Insert Greatest Common Divisors in Linked List.
+Memory Usage: 19.64 MB, less than 36.27% of Python3 online submissions for Insert Greatest Common Divisors in Linked List.
 """
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+
+        ptr = head
+        while ptr:
+            if ptr.next:
+                value = gcd(ptr.val, ptr.next.val)
+                ptr.next = ListNode(value, ptr.next)
+                ptr = ptr.next
+            ptr = ptr.next
+
+        return head
