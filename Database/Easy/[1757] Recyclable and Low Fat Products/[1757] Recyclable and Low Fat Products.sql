75cad3eb7163be6738af319e76af08cc35b1a54c
Samarth Jain <jsamarth84@gmail.com>
2024-09-19 16:13:53 +0530

    Leetcode-Solutions

--- Database/Easy/[1757] Recyclable and Low Fat Products/[1757] Recyclable and Low Fat Products.sql
+++ Database/Easy/[1757] Recyclable and Low Fat Products/[1757] Recyclable and Low Fat Products.sql
@@ -1,8 +1,8 @@
 /*
 Accepted
 1757 [Easy]
-Runtime: 1348 ms, faster than 22.01% of Oracle online submissions for Recyclable and Low Fat Products.
+Runtime: 463 ms, faster than 79.41% of MySQL online submissions for Recyclable and Low Fat Products.
 */
 
-/* Write your PL/SQL query statement below */
-SELECT product_id FROM Products WHERE low_fats='Y' and recyclable='Y';
+/* Write your MySQL query statement below */
+SELECT product_id FROM Products WHERE low_fats = 'Y' AND recyclable = 'Y'
