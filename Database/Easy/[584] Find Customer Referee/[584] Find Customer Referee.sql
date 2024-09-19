d8a6a7565d30ff6e5458494fdda2685ddef2d297
Samarth Jain <jsamarth84@gmail.com>
2024-09-19 16:25:51 +0530

    Leetcode-Solutions

--- Database/Easy/[584] Find Customer Referee/[584] Find Customer Referee.sql
+++ Database/Easy/[584] Find Customer Referee/[584] Find Customer Referee.sql
@@ -1,8 +1,8 @@
 /*
 Accepted
 584 [Easy]
-Runtime: 1020 ms, faster than 48.25% of Oracle online submissions for Find Customer Referee.
+Runtime: 429 ms, faster than 75.42% of MySQL online submissions for Find Customer Referee.
 */
 
-/* Write your PL/SQL query statement below */
-SELECT name FROM Customer WHERE referee_id!=2 OR referee_id IS null;
+/* Write your MySQL query statement below */
+SELECT name FROM Customer WHERE referee_id != 2 OR referee_id IS null
