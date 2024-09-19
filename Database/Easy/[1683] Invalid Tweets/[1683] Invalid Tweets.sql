8e9116ac45cffedcb20a30dea60ecd15e485092a
Samarth Jain <jsamarth84@gmail.com>
2024-09-19 16:41:24 +0530

    Leetcode-Solutions

--- Database/Easy/[1683] Invalid Tweets/[1683] Invalid Tweets.sql
+++ Database/Easy/[1683] Invalid Tweets/[1683] Invalid Tweets.sql
@@ -1,9 +1,10 @@
 /*
 Accepted
 1683 [Easy]
-Runtime: 1197 ms, faster than 36.72% of Oracle online submissions for Invalid Tweets.
+Runtime: 577 ms, faster than 54.46% of MySQL online submissions for Invalid Tweets.
 */
 
-/* Write your PL/SQL query statement below */
-SELECT tweet_id FROM Tweets
+/* Write your MySQL query statement below */
+SELECT tweet_id
+FROM Tweets
 WHERE LENGTH(content) > 15;
