0806e06b3c0752b95fd4fe6a1ac9b7e71fc94d91
Samarth Jain <jsamarth84@gmail.com>
2024-09-20 17:50:48 +0530

    Leetcode-Solutions

---
+++ Hard/[214] Shortest Palindrome/[214] Shortest Palindrome.py
@@ -0,0 +1,20 @@
+"""
+Accepted
+214 [Hard]
+Runtime: 777 ms, faster than 16.52% of Python3 online submissions for Shortest Palindrome.
+Memory Usage: 16.78 MB, less than 91.91% of Python3 online submissions for Shortest Palindrome.
+"""
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        def checkPalindrome(string):
+            temp = string
+            temp = temp[ : : -1]
+            if temp == string:
+                return True
+
+        for i in range(len(s)):
+            prefix = s[len(s) - i : ]
+            prefix = prefix[ : : -1]
+            if checkPalindrome(prefix + s):
+                return prefix + s
+        return ""
