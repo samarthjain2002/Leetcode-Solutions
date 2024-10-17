e526c64c6f6034cfb5ee7176b39b6598edf2f066
Samarth Jain <jsamarth84@gmail.com>
2024-10-17 13:10:50 +0530

    Leetcode-Solutions

--- Medium/[670] Maximum Swap/[670] Maximum Swap.py
+++ Medium/[670] Maximum Swap/[670] Maximum Swap.py
@@ -1,6 +1,40 @@
 """
 Accepted
 670 [Medium]
+Runtime: 29 ms, faster than 88.62% of Python3 online submissions for Maximum Swap.
+Memory Usage:  16.36 MB, less than 97.49% of Python3 online submissions for Maximum Swap.
+"""
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        arr = []
+        for dig in str(num):
+            arr.append(int(dig))
+        N = len(arr)
+
+        hashMap = defaultdict(int)
+        for i, dig in enumerate(arr):
+            hashMap[dig] = i
+
+        flag = False
+        for i, dig in enumerate(arr):
+            for j in range(9, -1, -1):
+                if j > dig and i < hashMap[j]:
+                    temp = dig
+                    arr[i] = j
+                    arr[hashMap[j]] = temp
+                    flag = True
+                    break
+            if flag:
+                break
+        
+        res = ""
+        for dig in arr:
+            res += str(dig)
+        return int(res)
+
+
+
+"""
 Runtime: 32 ms, faster than 74.49% of Python3 online submissions for Maximum Swap.
 Memory Usage:  16.54 MB, less than 42.30% of Python3 online submissions for Maximum Swap.
 """
