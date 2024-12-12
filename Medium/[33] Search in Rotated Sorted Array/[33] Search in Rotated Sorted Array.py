f3c1cc9f331802c8890fedc3e666a4621b724ae9
Samarth Jain <jsamarth84@gmail.com>
2024-12-12 21:59:22 +0530

    Leetcode-Solutions

--- Medium/[33] Search in Rotated Sorted Array/[33] Search in Rotated Sorted Array.py
+++ Medium/[33] Search in Rotated Sorted Array/[33] Search in Rotated Sorted Array.py
@@ -28,6 +28,5 @@ class Solution:
                 elif target < nums[mid]:
                     right = mid - 1
                 else:
-                    left = mid + 1
-                
+                    left = mid + 1     
         return -1
