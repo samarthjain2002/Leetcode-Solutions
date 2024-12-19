92a36fa10a3a106740d95f0673fae60f50eb90a0
Samarth Jain <jsamarth84@gmail.com>
2024-12-19 19:36:58 +0530

    Leetcode-Solutions

--- Medium/[146] LRU Cache/[146] LRU Cache.py
+++ Medium/[146] LRU Cache/[146] LRU Cache.py
@@ -1,8 +1,8 @@
 """
 Accepted
 146 [Medium]
-Runtime: 124 ms, faster than 75.17% of Python3 online submissions for LRU Cache.
-Memory Usage:  78.30 MB, less than 15.34% of Python3 online submissions for LRU Cache.
+Runtime: 124 ms, faster than 75.18% of Python3 online submissions for LRU Cache.
+Memory Usage:  78.30 MB, less than 15.44% of Python3 online submissions for LRU Cache.
 """
 class ListNode:
     def __init__(self, key, val, left = None, right = None):
@@ -14,69 +14,43 @@ class ListNode:
 
 class LRUCache:
     def __init__(self, capacity: int):
-        self.capacity = capacity
         self.cache = {}
-        self.ptr = None
+        self.capacity = capacity
+        self.head = ListNode(-1, -1)
+        self.tail = ListNode(-1, -1, self.head)
+        self.head.right = self.tail
+
+    def remove_node(self, node):
+        prev, nxt = node.left, node.right
+        prev.right, nxt.left = node.right, node.left
+
+    def add_node(self, node):
+        prev, nxt = self.tail.left, self.tail
+        prev.right = nxt.left = node
+        node.left, node.right = prev, nxt
 
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
-        
-        # If the most-recently used key is fetched again
-        if self.ptr.key == key:
-            return self.ptr.val
 
-        # If the fetched node has a left node
-        if self.cache[key].left:
-            self.cache[key].left.right = self.cache[key].right
-        self.cache[key].right.left = self.cache[key].left
+        self.remove_node(self.cache[key])
+        self.add_node(self.cache[key])
 
-        # Pointer points to the fetched node
-        self.ptr.right = self.cache[key]
-        self.cache[key].left = self.ptr
-        # Pointer becomes the fetched node
-        self.ptr = self.cache[key]
-
-        return self.ptr.val
+        return self.cache[key].val
 
     def put(self, key: int, value: int) -> None:
-        # If there is no node/cache is empty
-        if not self.ptr:
-            newNode = ListNode(key, value)
-            self.cache[key] = newNode
-            self.ptr = newNode
-            self.capacity -= 1
-        # If the key is in the cache
-        elif key in self.cache:
+        if key in self.cache:
             self.cache[key].val = value
-            # Moving the node to the right-most position is written in the get method
-            self.get(key)
-        # If there is space in the cache
-        elif self.capacity:
-            newNode = ListNode(key, value, self.ptr)
-            self.cache[key] = newNode
-            self.ptr.right = newNode
-            self.ptr = newNode
-            self.capacity -= 1
-        # If the capacity of the cache is exhausted
-        else:
-            # Cur pointer moves to the left-most node
-            cur = self.ptr
-            while cur.left:
-                cur = cur.left
-            # Left-most node is remvoed from the cache
-            del self.cache[cur.key]
-            
-            # If there are more than 1 nodes
-            if cur.right:
-                cur.right.left = None
-            # If there is only 1 node
-            else:
-                self.ptr = None
-
-            # Adding the the key-value pair to the right-most end
-            self.capacity += 1
-            self.put(key, value)
+            self.remove_node(self.cache[key])
+            self.add_node(self.cache[key])
+            return
+        elif len(self.cache) == self.capacity:
+            lru = self.head.right
+            self.remove_node(lru)
+            del self.cache[lru.key]
+        newNode = ListNode(key, value)
+        self.add_node(newNode)
+        self.cache[key] = newNode
 
 
 # Your LRUCache object will be instantiated and called as such:
