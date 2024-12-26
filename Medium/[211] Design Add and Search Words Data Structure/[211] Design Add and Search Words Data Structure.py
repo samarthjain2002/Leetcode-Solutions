d27a73b1b499fadf8594b06d3349cbd345483996
Samarth Jain <jsamarth84@gmail.com>
2024-12-26 12:38:54 +0530

    Leetcode-Solutions

---
+++ Medium/[211] Design Add and Search Words Data Structure/[211] Design Add and Search Words Data Structure.py
@@ -0,0 +1,6 @@
+"""
+Accepted
+211 [Medium]
+Runtime: 1302 ms, faster than 45.04% of Python3 online submissions for Design Add and Search Words Data Structure.
+Memory Usage:  65.46 MB, less than 27.83% of Python3 online submissions for Design Add and Search Words Data Structure.
+"""
