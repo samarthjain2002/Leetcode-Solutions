31a3b35a1cfe8e909917a2e5d44bdf7ae89c1db9
Samarth Jain <jsamarth84@gmail.com>
2024-12-30 16:08:33 +0530

    Leetcode-Solutions

--- Medium/[621] Task Scheduler/[621] Task Scheduler.py
+++ Medium/[621] Task Scheduler/[621] Task Scheduler.py
@@ -1,6 +1,33 @@
 """
 Accepted
 621 [Medium]
+Runtime: 140 ms, faster than 44.46% of Python3 online submissions for Task Scheduler.
+Memory Usage:  19.26 MB, less than 12.47% of Python3 online submissions for Task Scheduler.
+"""
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        freqMap = Counter(tasks)
+        maxHeap = [-num for num in freqMap.values()]
+        heapq.heapify(maxHeap)
+
+        time = 0
+        queue = deque()
+
+        while maxHeap or queue:
+            if maxHeap:
+                task = heapq.heappop(maxHeap)
+                task += 1
+                if task < 0:
+                    queue.append([task, time + n])
+            if queue and queue[0][1] == time:
+                task, t = queue.popleft()
+                heapq.heappush(maxHeap, task)
+            time += 1
+        return time
+
+
+
+"""
 Runtime: 357 ms, faster than 84.27% of Python3 online submissions for Task Scheduler.
 Memory Usage:  17.18 MB, less than 59.31% of Python3 online submissions for Task Scheduler.
 """
