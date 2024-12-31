b55e816e255bdca52ca53d4f66311468473f7dd4
Samarth Jain <jsamarth84@gmail.com>
2024-12-31 20:26:04 +0530

    Leetcode-Solutions

--- Medium/[78] Subsets/[78] Subsets.py
+++ Medium/[78] Subsets/[78] Subsets.py
@@ -22,3 +22,25 @@ class Solution:
 
         backtrack(0)
         return res
+
+
+
+"""
+Runtime: 0 ms, faster than 100.00% of Python3 online submissions for Subsets.
+Memory Usage:  18.06 MB, less than 8.25% of Python3 online submissions for Subsets.
+"""
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+        def helper(i, num):
+            if i >= len(nums):
+                return
+
+            res.add(tuple(num))
+            res.add(tuple(num + [nums[i]]))
+
+            helper(i + 1, num)
+            helper(i + 1, num + [nums[i]])
+
+        helper(0, [])
+        return [list(subset) for subset in res]
