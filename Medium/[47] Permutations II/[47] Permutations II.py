225f7d8574e51337e377fff7e210acbd5a92439a
Samarth Jain <jsamarth84@gmail.com>
2024-12-31 20:16:27 +0530

    Leetcode-Solutions

--- Medium/[47] Permutations II/[47] Permutations II.py
+++ Medium/[47] Permutations II/[47] Permutations II.py
@@ -12,3 +12,50 @@ class Solution:
         hashSet = set(perm)
         perm = [list(p) for p in hashSet]
         return perm
+    
+
+
+"""
+Runtime: 4 ms, faster than 74.67% of Python3 online submissions for Permutations.
+Memory Usage: 17.94 MB, less than 22.50% of Python3 online submissions for Permutations.
+"""
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(nums):
+            if len(nums) == 0:
+                return [[]]
+
+            perms = backtrack(nums[1 : ])
+            res = set()
+
+            for perm in perms:
+                for i in range(len(perm) + 1):
+                    perm_copy = perm.copy()
+                    perm_copy.insert(i, nums[0])
+                    res.add(tuple(perm_copy))
+
+            return [list(subset) for subset in res]
+
+        return backtrack(nums)
+
+
+
+"""
+Runtime: 3 ms, faster than 94.14% of Python3 online submissions for Permutations.
+Memory Usage: 18.26 MB, less than 7.39% of Python3 online submissions for Permutations.
+"""
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 0:
+            return [[]]
+
+        perms = self.permuteUnique(nums[1 : ])
+        res = set()
+
+        for perm in perms:
+            for i in range(len(perm) + 1):
+                perm_copy = perm.copy()
+                perm_copy.insert(i, nums[0])
+                res.add(tuple(perm_copy))
+
+        return [list(subset) for subset in res]
