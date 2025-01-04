462bef94aa43d9b5d9cc541cdea147d81057a16f
Samarth Jain <jsamarth84@gmail.com>
2025-01-04 22:22:34 +0530

    Leetcode-Solutions

--- Medium/[62] Unique Paths/[62] Unique Paths.py
+++ Medium/[62] Unique Paths/[62] Unique Paths.py
@@ -1,21 +1,20 @@
 """
 Accepted
 62 [Medium]
-Runtime: 38 ms, faster than 40.69% of Python3 online submissions for Unique Paths.
-Memory Usage: 16.59 MB, less than 41.63% of Python3 online submissions for Unique Paths.
+Runtime: 0 ms, faster than 100.00% of Python3 online submissions for Unique Paths.
+Memory Usage: 17.98 MB, less than 7.28% of Python3 online submissions for Unique Paths.
 """
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[0] * n] * m
-        dp[0][0] = 1
-        for i in range(m):
-            for j in range(n):
-                if i == 0 and j == 0:
-                    pass
-                elif i == 0:
-                    dp[i][j] = dp[i][j - 1]
-                elif j == 0:
-                    dp[i][j] = dp[i - 1][j]
+        dp = [[1] * n for _ in range(m)]
+        for row in range(m - 1, -1, -1):
+            for col in range(n - 1, -1, -1):
+                if row + 1 >= m and col + 1 >= n:
+                    dp[row][col] = 1
+                elif row + 1 >= m:
+                    dp[row][col] = dp[row][col + 1] + 0
+                elif col + 1 >= n:
+                    dp[row][col] = dp[row + 1][col] + 0
                 else:
-                    dp[i][j] = dp[i - 1][j] + dp[i][j -1]
-        return dp[m - 1][n - 1]
+                    dp[row][col] = dp[row][col + 1] + dp[row + 1][col]
+        return dp[0][0]
