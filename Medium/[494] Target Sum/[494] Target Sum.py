ee69622d81bb34740298fbb6f397053bcba1176b
Samarth Jain <jsamarth84@gmail.com>
2025-01-07 20:37:10 +0530

    Leetcode-Solutions

---
+++ Medium/[494] Target Sum/[494] Target Sum.py
@@ -0,0 +1,24 @@
+"""
+Accepted
+494 [Medium]
+Runtime: 191 ms, faster than 30.81% of Python3 online submissions for Target Sum.
+Memory Usage:  38.50 MB, less than 27.86% of Python3 online submissions for Target Sum.
+"""
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        N = len(nums)
+        dp = {}
+
+        def rec(i, amount):
+            if i >= N:
+                if amount == target:
+                    return 1
+                else:
+                    return 0
+            if (i, amount) in dp:
+                return dp[(i, amount)]
+
+            dp[(i, amount)] = rec(i + 1, amount + nums[i]) + rec(i + 1, amount - nums[i])
+            return dp[(i, amount)]
+
+        return rec(0, 0)
---
+++ Medium/[518] Coin Change II/[518] Coin Change II.py
@@ -0,0 +1,28 @@
+"""
+Accepted
+518 [Medium]
+Runtime: 394 ms, faster than 38.90% of Python3 online submissions for Coin Change II.
+Memory Usage:  48.25 MB, less than 45.03% of Python3 online submissions for Coin Change II.
+"""
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        N = len(coins)
+        # COLS: 0 to amount, ROWS: coins
+        dp = [[0] * (amount + 1) for _ in range(N)]
+
+        # I can make 0 amount without picking any coin, so 1 denomination
+        for i in range(N):
+            dp[i][0] = 1
+
+        for i in range(N - 1, -1, -1):
+            for a in range(1, amount + 1):
+                # If I choose to pick the coin
+                pick = a - coins[i]
+                if pick >= 0:
+                    dp[i][a] = dp[i][pick]
+                # If I don't choose to pick the coin
+                if i < N - 1:
+                    dp[i][a] += dp[i + 1][a]
+                # Add them both
+                
+        return dp[0][-1]
