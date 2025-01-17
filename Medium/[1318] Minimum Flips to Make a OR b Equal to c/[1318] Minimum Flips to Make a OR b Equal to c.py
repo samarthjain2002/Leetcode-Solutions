0fcd7db4d397ecada5a7287e05bca331cd052ea8
Samarth Jain <jsamarth84@gmail.com>
2025-01-17 18:11:14 +0530

    Leetcode-Solutions

---
+++ Medium/[1318] Minimum Flips to Make a OR b Equal to c/[1318] Minimum Flips to Make a OR b Equal to c.py
@@ -0,0 +1,18 @@
+"""
+Accepted
+1318 [Medium]
+Runtime: 34 ms, faster than 72.09% of Python3 online submissions for Minimum Flips to Make a OR b Equal to c.
+Memory Usage:  17.67 MB, less than 39.78% of Python3 online submissions for Minimum Flips to Make a OR b Equal to c.
+"""
+class Solution:
+    def minFlips(self, a: int, b: int, c: int) -> int:
+        res = 0
+        for i in range(30):
+            if not (c & (1 << i)):
+                if a & (1 << i):
+                    res += 1
+                if b & (1 << i):
+                    res += 1
+            elif (not (a & (1 << i))) and (not (b & (1 << i))):
+                res += 1
+        return res
