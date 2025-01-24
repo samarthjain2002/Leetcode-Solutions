f814fa23311402d33b2af9ce09f8fe7e0ac2f12f
Samarth Jain <jsamarth84@gmail.com>
2025-01-24 20:39:03 +0530

    Leetcode-Solutions

---
+++ Medium/[802] Find Eventual Safe States/[802] Find Eventual Safe States.py
@@ -0,0 +1,27 @@
+"""
+Accepted
+802 [Medium]
+Runtime: 46 ms, faster than 55.92% of Python3 online submissions for Find Eventual Safe States.
+Memory Usage:  23.70 MB, less than 39.31% of Python3 online submissions for Find Eventual Safe States.
+"""
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        N = len(graph)
+        safe = {}
+
+        def isSafe(node):
+            if node in safe:
+                return safe[node]
+
+            safe[node] = False
+            for nei in graph[node]:
+                if not isSafe(nei):
+                    return False
+            safe[node] = True
+            return True
+
+        res = []
+        for i in range(N):
+            if isSafe(i):
+                res.append(i)
+        return res
