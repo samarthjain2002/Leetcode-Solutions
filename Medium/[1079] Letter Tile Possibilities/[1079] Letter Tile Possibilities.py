4efe3601bae860ea64c97828a6b84046877f23bd
Samarth Jain <jsamarth84@gmail.com>
2025-02-17 09:54:58 +0530

    Leetcode-Solutions

--- Medium/[1079] Letter Tile Possibilities/[1079] Letter Tile Possibilities.py
+++ Medium/[1079] Letter Tile Possibilities/[1079] Letter Tile Possibilities.py
@@ -1,8 +1,8 @@
 """
 Accepted
 1079 [Medium]
-Runtime: 79 ms, faster than 14.91% of Python3 online submissions for Letter Tile Possibilities.
-Memory Usage:  26.93 MB, less than 12.87% of Python3 online submissions for Letter Tile Possibilities.
+Runtime: 35 ms, faster than 64.77% of Python3 online submissions for Letter Tile Possibilities.
+Memory Usage:  26.89 MB, less than 15.04% of Python3 online submissions for Letter Tile Possibilities.
 """
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
@@ -10,7 +10,8 @@ class Solution:
         def backtrack(string, lis):
             for i in range(len(lis)):
                 string += lis[i]
-                backtrack(string, lis[ : i] + lis[i + 1 : ])
+                if string not in res:
+                    backtrack(string, lis[ : i] + lis[i + 1 : ])
                 res.add(string)
                 string = string[:-1]
 
