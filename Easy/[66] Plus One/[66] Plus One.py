a2d758f97999ea21535d4baa9d8ddace08ab78c1
Samarth Jain <jsamarth84@gmail.com>
2025-02-25 14:11:10 +0530

    Leetcode-Solutions

--- Easy/[66] Plus One/[66] Plus One.py
+++ Easy/[66] Plus One/[66] Plus One.py
@@ -1,16 +1,19 @@
 """
 Accepted
 66 [Easy]
-Runtime: 39 ms, faster than 40.34% of Python3 online submissions for Plus One.
-Memory Usage: 16.52 MB, less than 40.06% of Python3 online submissions for Plus One.
+Runtime: 0 ms, faster than 100.00% of Python3 online submissions for Plus One.
+Memory Usage: 17.73 MB, less than 49.65% of Python3 online submissions for Plus One.
 """
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        num = ""
-        for i in digits:
-            num += str(i)
-        num = str(int(num) + 1)
-        digits = []
-        for i in num:
-            digits.append(int(i))
-        return digits
+        res = [0] + digits
+
+        carry = True
+        for i in range(len(res) - 1, -1, -1):
+            if carry:
+                if res[i] == 9:
+                    res[i] = 0
+                else:
+                    res[i] += 1
+                    break
+        return res if res[0] == 1 else res[1 : ]
