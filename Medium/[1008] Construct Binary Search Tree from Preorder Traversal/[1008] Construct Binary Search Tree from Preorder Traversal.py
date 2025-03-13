bd29d1a2c2272a541c20e9cd724eec461cb8ed58
Samarth Jain <jsamarth84@gmail.com>
2025-03-13 08:15:43 +0530

    Leetcode-Solutions

---
+++ Medium/[1008] Construct Binary Search Tree from Preorder Traversal/[1008] Construct Binary Search Tree from Preorder Traversal.py
@@ -0,0 +1,31 @@
+"""
+Accepted
+1008 [Medium]
+Runtime: 2 ms, faster than 25.18% of Python online submissions for Construct Binary Search Tree from Preorder Traversal.
+Memory Usage: 17.73 MB, less than 67.69% of Python online submissions for Construct Binary Search Tree from Preorder Traversal.
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
+        root = TreeNode(preorder[0])
+
+        def dfs(root, node):
+            if node < root.val:
+                if not root.left:
+                    root.left = TreeNode(node)
+                else:
+                    dfs(root.left, node)
+            else:
+                if not root.right:
+                    root.right = TreeNode(node)
+                else:
+                    dfs(root.right, node)
+        
+        for node in preorder[1 : ]:
+            dfs(root, node)
+        return root
