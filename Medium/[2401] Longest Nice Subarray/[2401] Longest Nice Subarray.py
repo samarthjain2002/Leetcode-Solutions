18bdf6186948a32f4c26333fb4217d3787bd8864
Samarth Jain <jsamarth84@gmail.com>
2025-03-18 22:19:39 +0530

    Leetcode-Solutions

---
+++ Medium/[2401] Longest Nice Subarray/[2401] Longest Nice Subarray.py
@@ -0,0 +1,6 @@
+"""
+Accepted
+2401 [Medium]
+Runtime: 19 ms, faster than 37.19% of Python3 online submissions for Longest Nice Subarray.
+Memory Usage: 20.61 MB, less than 24.43% of Python3 online submissions for Longest Nice Subarray.
+"""
