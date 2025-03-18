376c96a2529a8b87ca663108440cb6cc2342a577
Samarth Jain <jsamarth84@gmail.com>
2025-03-18 20:36:20 +0530

    Leetcode-Solutions

--- Medium/[64] Minimum Path Sum/[64] Minimum Path Sum.py
+++ Medium/[64] Minimum Path Sum/[64] Minimum Path Sum.py
@@ -1,9 +1,33 @@
 """
 Accepted
 64 [Medium]
+Runtime: 19 ms, faster than 37.19% of Python3 online submissions for Minimum Path Sum.
+Memory Usage: 20.61 MB, less than 24.43% of Python3 online submissions for Minimum Path Sum.
+"""
+# Bottom-Up Tabulation approach
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        dp = [[0] * len(grid[0]) for _ in range(len(grid))]
+
+        for i in range(len(grid) - 1, -1, -1):
+            for j in range(len(grid[0]) - 1, -1, -1):
+                if i == len(grid) - 1 and j == len(grid[0]) - 1:
+                    dp[i][j] = grid[i][j]
+                elif i == len(grid) - 1:
+                    dp[i][j] = dp[i][j + 1] + grid[i][j]
+                elif j == len(grid[0]) - 1:
+                    dp[i][j] = dp[i + 1][j] + grid[i][j]
+                else:
+                    dp[i][j] = min(dp[i][j + 1], dp[i + 1][j]) + grid[i][j]
+        return dp[0][0]
+
+
+
+"""
 Runtime: 81 ms, faster than 5.30% of Python3 online submissions for Minimum Path Sum.
 Memory Usage: 28.29 MB, less than 8.93% of Python3 online submissions for Minimum Path Sum.
 """
+# Top-Down Memoization approach
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         cache = {}
