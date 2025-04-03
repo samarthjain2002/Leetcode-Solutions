52390c9fbf9781bd921f7f4d6d73b729e9fa0d2d
Samarth Jain <jsamarth84@gmail.com>
2025-04-03 12:43:19 +0530

    Leetcode-Solutions

--- Easy/[2873] Maximum Value of an Ordered Triplet I/[2873] Maximum Value of an Ordered Triplet I.py
+++ Easy/[2873] Maximum Value of an Ordered Triplet I/[2873] Maximum Value of an Ordered Triplet I.py
@@ -1,9 +1,27 @@
 """
 Accepted
 2873 [Easy]
+Runtime: 12 ms, faster than 58.58% of Python3 online submissions for Maximum Value of an Ordered Triplet I.
+Memory Usage: 17.77 MB, less than 54.41% of Python3 online submissions for Maximum Value of an Ordered Triplet I.
+"""
+# TC: O(n^2)
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        res = 0
+        i = 0
+        for j in range(len(nums)):
+            for k in range(j + 1, len(nums)):
+                i = j if nums[j] > nums[i] else i
+                res = max(res, (nums[i] - nums[j]) * nums[k])
+        return res
+
+
+
+"""
 Runtime: 160 ms, faster than 18.95% of Python3 online submissions for Maximum Value of an Ordered Triplet I.
 Memory Usage: 17.62 MB, less than 82.79% of Python3 online submissions for Maximum Value of an Ordered Triplet I.
 """
+# TC: O(n^3)
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
         res = 0
