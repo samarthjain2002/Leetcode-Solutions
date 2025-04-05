f8d170b5430a7a0293f6de31ef77a77b9419df52
Samarth Jain <jsamarth84@gmail.com>
2025-04-05 12:15:13 +0530

    Leetcode-Solutions

---
+++ Easy/[1025] Divisor Game/[1025] Divisor Game.py
@@ -0,0 +1,9 @@
+"""
+Accepted
+1025 [Easy]
+Runtime: 0 ms, faster than 100.00% of Python3 online submissions for Divisor Game.
+Memory Usage: 17.68 MB, less than 82.11% of Python3 online submissions for Divisor Game.
+"""
+class Solution:
+    def divisorGame(self, n: int) -> bool:
+        return n % 2 == 0
