5655d2b41de5f351e3130c1a35d625f2de1a0102
Samarth Jain <jsamarth84@gmail.com>
2025-04-07 16:51:26 +0530

    Leetcode-Solutions

--- Database/Easy/[1789] Primary Department for Each Employee/[1789] Primary Department for Each Employee.sql
+++ Database/Easy/[1789] Primary Department for Each Employee/[1789] Primary Department for Each Employee.sql
@@ -1,6 +1,14 @@
 /*
 Accepted
-1789 [Medium]
-Runtime: 1391 ms, faster than 71.49% of MySQL online submissions for Primary Department for Each Employee.
+1789 [Easy]
+Runtime: 515 ms, faster than 96.84% of MySQL online submissions for Primary Department for Each Employee.
 */
 
+# Write your MySQL query statement below
+SELECT employee_id, department_id
+FROM Employee
+GROUP BY employee_id HAVING COUNT(*) = 1
+    UNION
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag = 'Y'
