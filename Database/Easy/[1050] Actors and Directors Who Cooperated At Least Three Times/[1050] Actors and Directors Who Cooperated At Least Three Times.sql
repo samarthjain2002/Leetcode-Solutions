af3b109c5d2963c3edad05bcea9544edd64c9748
Samarth Jain <jsamarth84@gmail.com>
2025-04-11 14:07:11 +0530

    Leetcode-Solutions

---
+++ Database/Easy/[1050] Actors and Directors Who Cooperated At Least Three Times/[1050] Actors and Directors Who Cooperated At Least Three Times.sql
@@ -0,0 +1,10 @@
+/*
+Accepted
+1050 [Medium]
+Runtime: 290 ms, faster than 94.84% of PostgreSQL online submissions for Actors and Directors Who Cooperated At Least Three Times.
+*/
+
+-- Write your PostgreSQL query statement below
+SELECT actor_id, director_id
+FROM ActorDirector
+GROUP BY actor_id, director_id HAVING COUNT(*) >= 3
