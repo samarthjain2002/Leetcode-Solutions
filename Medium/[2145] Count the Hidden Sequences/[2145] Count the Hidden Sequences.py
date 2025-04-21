2a95651407231a0b3948e34c91685be05369b65e
Samarth Jain <jsamarth84@gmail.com>
2025-04-21 13:04:48 +0530

    Leetcode-Solutions

--- Medium/[2145] Count the Hidden Sequences/[2145] Count the Hidden Sequences.py
+++ Medium/[2145] Count the Hidden Sequences/[2145] Count the Hidden Sequences.py
@@ -1,6 +1,22 @@
 """
 Accepted
 2145 [Medium]
+Runtime: 167 ms, faster than 19.19% of Python3 online submissions for Count the Hidden Sequences.
+Memory Usage: 32.31 MB, less than 57.58% of Python3 online submissions for Count the Hidden Sequences.
+"""
+class Solution:
+    def numberOfArrays(self, differences: List[int], lower: int, upper: int) -> int:
+        maximum = minimum = curSum = 0
+        for i in range(len(differences)):
+            curSum += differences[i]
+            maximum = max(maximum, curSum)
+            minimum = min(minimum, curSum)
+            
+        return max(0, (upper - lower) - (maximum - minimum) + 1)
+
+
+
+"""
 Runtime: 477 ms, faster than 5.05% of Python3 online submissions for Count the Hidden Sequences.
 Memory Usage: 32.31 MB, less than 57.58% of Python3 online submissions for Count the Hidden Sequences.
 """
