65dbf05d5574071a004d0e775735ede9372107c0
Samarth Jain <jsamarth84@gmail.com>
2025-04-30 17:34:33 +0530

    Leetcode-Solutions

--- Medium/[901] Online Stock Span/[901] Online Stock Span.py
+++ Medium/[901] Online Stock Span/[901] Online Stock Span.py
@@ -1,6 +1,29 @@
 """
 Accepted
 901 [Medium]
+Runtime: 40 ms, faster than 96.80% of Python online submissions for Online Stock Span.
+Memory Usage: 22.25 MB, less than 86.07% of Python online submissions for Online Stock Span.
+"""
+class StockSpanner:
+
+    def __init__(self):
+        self.stack = []
+
+    def next(self, price: int) -> int:
+        span = 1
+        while self.stack and self.stack[-1][0] <= price:
+            span += self.stack.pop()[1]
+        self.stack.append([price, span])
+        return span
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
+
+
+
+"""
 Runtime: 8113 ms, faster than 5.02% of Python online submissions for Online Stock Span.
 Memory Usage: 22.12 MB, less than 14.97% of Python online submissions for Online Stock Span.
 """
