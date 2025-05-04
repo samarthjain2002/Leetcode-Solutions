f321e296b6bb181e6b68aa36f0f45f8f2d4a5468
Samarth Jain <jsamarth84@gmail.com>
2025-05-04 15:28:58 +0530

    Leetcode-Solutions

--- Easy/[1128] Number of Equivalent Domino Pairs/[1128] Number of Equivalent Domino Pairs.py
+++ Easy/[1128] Number of Equivalent Domino Pairs/[1128] Number of Equivalent Domino Pairs.py
@@ -1,9 +1,30 @@
 """
 Accepted
 1128 [Easy]
+Runtime: 2 ms, faster than 99.71% of Python3 online submissions for Number of Equivalent Domino Pairs.
+Memory Usage: 24.07 MB, less than 98.57% of Python3 online submissions for Number of Equivalent Domino Pairs.
+"""
+# TC: O(n), SC: O(1)
+class Solution:
+    def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
+        domId = [0] * 100
+        res = 0
+        for a, b in dominoes:
+            # Turn any domino (a, b) or (b, a) into same ID
+            ID = a * 10 + b if a >= b else b * 10 + a
+            # Add the previous count to result
+            res += domId[ID]
+            # Increment the count of ID pairs
+            domId[ID] += 1
+        return res
+
+
+
+"""
 Runtime: 7 ms, faster than 92.01% of Python3 online submissions for Number of Equivalent Domino Pairs.
 Memory Usage: 24.33 MB, less than 43.37% of Python3 online submissions for Number of Equivalent Domino Pairs.
 """
+# TC: O(n), SC: O(n)
 class Solution:
     def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
         hashMap = defaultdict(int)
