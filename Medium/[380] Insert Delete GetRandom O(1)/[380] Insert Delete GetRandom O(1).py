f3195e036aa10192cf6e00a9cb8214ebc6a56cbc
Samarth Jain <jsamarth84@gmail.com>
2025-05-19 18:20:53 +0530

    LeetCode-Solutions

--- Medium/[380] Insert Delete GetRandom O(1)/[380] Insert Delete GetRandom O(1).py
+++ Medium/[380] Insert Delete GetRandom O(1)/[380] Insert Delete GetRandom O(1).py
@@ -1,28 +1,39 @@
 """
 Accepted
 380 [Medium]
-Runtime: 474 ms, faster than 30.81% of Python3 online submissions for Insert Delete GetRandom O(1).
-Memory Usage:  64.68 MB, less than 19.71% of Python3 online submissions for Insert Delete GetRandom O(1).
+Runtime: 146 ms, faster than 44.50% of Python3 online submissions for Insert Delete GetRandom O(1).
+Memory Usage: 58.01 MB, less than 20.16% of Python3 online submissions for Insert Delete GetRandom O(1).
 """
 class RandomizedSet:
 
     def __init__(self):
-        self.randomSet = set()        
+        self.dict = {}
+        self.array = []  
 
     def insert(self, val: int) -> bool:
-        if val in self.randomSet:
+        if val in self.dict.keys():
             return False
-        self.randomSet.add(val)
+
+        self.dict[val] = len(self.array)
+        self.array.append(val)
         return True
 
     def remove(self, val: int) -> bool:
-        if val not in self.randomSet:
+        if val not in self.dict.keys():
             return False
-        self.randomSet.remove(val)
+
+        index = self.dict[val]
+        # Change the index of last element in array
+        self.dict[self.array[-1]] = index
+        # Switch the val to be removed with last element
+        self.array[index] = self.array[-1]
+        self.array.pop()
+        del self.dict[val]
         return True
 
     def getRandom(self) -> int:
-        return random.choice(list(self.randomSet))
+        index = random.randrange(len(self.array))
+        return self.array[index]
 
 
 # Your RandomizedSet object will be instantiated and called as such:
