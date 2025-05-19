f8102b6a6abb88c14c4268abfa2efb1ebfede523
Samarth Jain <jsamarth84@gmail.com>
2025-05-19 20:36:14 +0530

    LeetCode-Solutions

--- Medium/[1094] Car Pooling/[1094] Car Pooling.py
+++ Medium/[1094] Car Pooling/[1094] Car Pooling.py
@@ -1,6 +1,31 @@
 """
 Accepted
 1094 [Medium]
+Runtime: 0 ms, faster than 100.00% of Python3 online submissions for Car Pooling.
+Memory Usage: 18.11 MB, less than 74.79% of Python3 online submissions for Car Pooling.
+"""
+# Brute-force approach
+# TC: O(n), SC: O(n)
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        # Sort based on starting times
+        trips.sort(key = lambda trip: trip[1])
+        
+        changePassengers = [0] * 1001
+        for trip in trips:
+            changePassengers[trip[1]] += trip[0]
+            changePassengers[trip[2]] -= trip[0]
+
+        curPassengers = 0
+        for time in range(1001):
+            curPassengers += changePassengers[time]
+            if curPassengers > capacity:
+                return False
+        return True
+
+
+
+"""
 Runtime: 4 ms, faster than 34.08% of Python3 online submissions for Car Pooling.
 Memory Usage: 18.15 MB, less than 74.79% of Python3 online submissions for Car Pooling.
 """
