7b237b8f54f28de8b768feec0d57819bebb8b6dc
Samarth Jain <jsamarth84@gmail.com>
2025-05-23 11:38:06 +0530

    LeetCode-Solutions

--- Medium/[841] Keys and Rooms/[841] Keys and Rooms.py
+++ Medium/[841] Keys and Rooms/[841] Keys and Rooms.py
@@ -4,6 +4,7 @@ Accepted
 Runtime: 0 ms, faster than 100.00% of Python3 online submissions for Keys and Rooms.
 Memory Usage: 18.15 MB, less than 74.04% of Python3 online submissions for Keys and Rooms.
 """
+# Recursive DFS approach
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         visited = set()
@@ -16,6 +17,29 @@ class Solution:
                 dfs(key)
 
         dfs(0)
+        return len(visited) == len(rooms)
+
+
+
+"""
+Accepted
+841 [Medium]
+Runtime: 0 ms, faster than 100.00% of Python3 online submissions for Keys and Rooms.
+Memory Usage: 18.18 MB, less than 74.04% of Python3 online submissions for Keys and Rooms.
+"""
+# Iterative approach
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        visited = set([0])
+        keys_have = rooms[0]
+        while keys_have:
+            for i in range(len(keys_have)):
+                key = keys_have.pop()
+                if key not in visited:
+                    visited.add(key)
+                    for new_key in rooms[key]:
+                        keys_have.append(new_key)
+        
         return len(visited) == len(rooms)
 
 
