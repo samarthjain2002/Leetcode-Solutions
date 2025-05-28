1148f953f2e269fb469d8748f28cb14c555ab772
Samarth Jain <jsamarth84@gmail.com>
2025-05-28 12:41:56 +0530

    LeetCode-Solutions

---
+++ Medium/[162] Find Peak Element/Python3 Solution/[162] Find Peak Element.py
@@ -0,0 +1,19 @@
+"""
+Accepted
+162 [Medium]
+Runtime: 59 ms, faster than 36.27% of Python3 online submissions for Find Peak Element.
+Memory Usage:  20.48 MB, less than 62.18% of Python3 online submissions for Find Peak Element.
+"""
+# Binary Search approach
+# TC: O(logn), SC: O(1)
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            mid = low + ((high - low) // 2)
+            if (mid == 0 or nums[mid - 1] < nums[mid]) and (mid == len(nums) - 1 or nums[mid] > nums[mid + 1]):
+                return mid
+            elif mid == 0 or nums[mid + 1] > nums[mid]:
+                low = mid + 1
+            else:
+                high = mid - 1
