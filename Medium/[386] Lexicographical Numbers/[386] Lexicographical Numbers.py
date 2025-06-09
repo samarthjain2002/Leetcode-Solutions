88be82190278e9c4d3fcc57abc855c1ff927c943
Samarth Jain <jsamarth84@gmail.com>
2025-06-09 12:18:11 +0530

    LeetCode-Solutions

--- Medium/[386] Lexicographical Numbers/[386] Lexicographical Numbers.py
+++ Medium/[386] Lexicographical Numbers/[386] Lexicographical Numbers.py
@@ -1,6 +1,28 @@
 """
 Accepted
 386 [Medium]
+Runtime: 12 ms, faster than 62.06% of Python3 online submissions for Lexicographical Numbers.
+Memory Usage: 21.15 MB, less than 92.91% of Python3 online submissions for Lexicographical Numbers.
+"""
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        res = []
+        curr = 1
+        while len(res) < n:
+            res.append(curr)
+
+            if curr * 10 <= n:
+                curr = curr * 10
+            else:
+                while curr % 10 == 9 or curr == n:
+                    curr = curr // 10
+                curr += 1
+                
+        return res
+
+
+
+"""
 Runtime: 156 ms, faster than 5.06% of Python3 online submissions for Lexicographical Numbers.
 Memory Usage: 22.79 MB, less than 29.24% of Python3 online submissions for Lexicographical Numbers.
 """
