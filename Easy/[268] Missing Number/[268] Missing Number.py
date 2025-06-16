9fafb84be0b616cd56331d1bfc855c052e42d409
Samarth Jain <jsamarth84@gmail.com>
2025-06-16 13:33:43 +0530

    LeetCode-Solutions

--- Easy/[268] Missing Number/[268] Missing Number.py
+++ Easy/[268] Missing Number/[268] Missing Number.py
@@ -1,12 +1,71 @@
 """
 Accepted
 268 [Easy]
-Runtime: 1028 ms, faster than 14.11% of Python3 online submissions for Missing Number.
-Memory Usage: 17.84 MB, less than 49.34% of Python3 online submissions for Missing Number.
+Runtime: 0 ms, faster than 100.00% of Python3 online submissions for Missing Number.
+Memory Usage: 18.59 MB, less than 92.16% of Python3 online submissions for Missing Number.
 """
+# Math approach
+# TC: O(1), SC: O(1)
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        req_sum = (len(nums) * (len(nums) + 1)) // 2
+        actual_sum = sum(nums)
+        return req_sum - actual_sum
+
+
+
+"""
+Runtime: 3 ms, faster than 63.88% of Python3 online submissions for Missing Number.
+Memory Usage: 18.60 MB, less than 92.16% of Python3 online submissions for Missing Number.
+"""
+# Bit Manipulation/XOR approach
+# TC: O(n), SC: O(1)
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        xor = 0
+        for i in range(len(nums) + 1):
+            xor = xor ^ i
+
         for num in nums:
-            if num != 0 and num - 1 not in nums:
-                return num - 1
-        return len(nums)
+            xor = xor ^ num
+
+        return xor
+
+
+
+"""
+Runtime: 12 ms, faster than 21.47% of Python3 online submissions for Missing Number.
+Memory Usage: 18.76 MB, less than 52.15% of Python3 online submissions for Missing Number.
+"""
+# Binary Search approach
+# TC: O(nlog(n)), SC: (1)
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+
+        low, high = 0, len(nums) 
+        while low < high:
+            mid = low + ((high - low) // 2)
+
+            # nums[mid] can either be equal or greater than mid, never lesser
+            if nums[mid] != mid:
+                high = mid
+            else:
+                low = mid + 1
+
+        # Here, low = high
+        return low
+
+
+
+"""
+Runtime: 1303 ms, faster than 7.07% of Python3 online submissions for Missing Number.
+Memory Usage: 18.64 MB, less than 72.61% of Python3 online submissions for Missing Number.
+"""
+# Brute-force approach
+# TC: O(n^2), SC: O(1)
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        for i in range(len(nums) + 1):
+            if i not in nums:
+                return i
