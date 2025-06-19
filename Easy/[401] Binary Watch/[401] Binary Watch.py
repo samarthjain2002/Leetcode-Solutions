3dc3008b3ba91bb915446662616332d69e8694fb
Samarth Jain <jsamarth84@gmail.com>
2025-06-19 12:19:27 +0530

    LeetCode-Solutions

--- Easy/[401] Binary Watch/[401] Binary Watch.py
+++ Easy/[401] Binary Watch/[401] Binary Watch.py
@@ -1,6 +1,26 @@
 """
 Accepted
 401 [Easy]
+Runtime: 3 ms, faster than 34.33% of Python3 online submissions for Binary Watch.
+Memory Usage: 17.93 MB, less than 25.58% of Python3 online submissions for Binary Watch.
+"""
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        if turnedOn > 8:
+            return []
+
+        res = []
+        for hr in range(12):
+            for mins in range(60):
+                if bin(hr).count('1') + bin(mins).count('1') == turnedOn:
+                    if mins < 10:
+                        mins = "0" + str(mins)
+                    res.append(f"{hr}:{mins}")
+        return res
+
+
+
+"""
 Runtime: 0 ms, faster than 100.00% of Python3 online submissions for Binary Watch.
 Memory Usage: 17.88 MB, less than 43.04% of Python3 online submissions for Binary Watch.
 """
