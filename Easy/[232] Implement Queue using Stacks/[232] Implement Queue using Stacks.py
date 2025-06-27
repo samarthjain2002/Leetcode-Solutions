4f366e79eb9beb8d84832588f7c4afadc2091320
Samarth Jain <jsamarth84@gmail.com>
2025-06-27 19:55:25 +0530

    LeetCode-Solutions

--- Easy/[232] Implement Queue using Stacks/[232] Implement Queue using Stacks.py
+++ Easy/[232] Implement Queue using Stacks/[232] Implement Queue using Stacks.py
@@ -1,28 +1,36 @@
 """
 Accepted
 232 [Easy]
-Runtime: 37 ms, faster than 56.95% of Python3 online submissions for Implement Queue using Stacks.
-Memory Usage: 16.50 MB, less than 80.67% of Python3 online submissions for Implement Queue using Stacks.
+Runtime: 0 ms, faster than 100.00% of Python3 online submissions for Implement Queue using Stacks.
+Memory Usage: 17.80 MB, less than 64.28% of Python3 online submissions for Implement Queue using Stacks.
 """
 class MyQueue:
 
     def __init__(self):
-        self.stack = []
+        self.stack1 = []
+        self.stack2 = []
 
     def push(self, x: int) -> None:
-        self.stack.insert(0, x)
+        self.stack1.append(x)
 
     def pop(self) -> int:
-        return self.stack.pop()
+        if self.stack2:
+            return self.stack2.pop()
+
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
 
     def peek(self) -> int:
-        return self.stack[-1]
+        if self.stack2:
+            return self.stack2[-1]
+
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        return self.stack2[-1]
 
     def empty(self) -> bool:
-        if self.stack:
-            return False
-        else:
-            return True
+        return not self.stack1 and not self.stack2
 
 
 # Your MyQueue object will be instantiated and called as such:
