39bb8f60c08ff07c257451fba8047ddfdff6204e
Samarth Jain <jsamarth84@gmail.com>
2025-08-14 09:04:38 +0530

    LeetCode-Solutions

--- Easy/[2264] Largest 3-Same-Digit Number in String/[2264] Largest 3-Same-Digit Number in String.py
+++ Easy/[2264] Largest 3-Same-Digit Number in String/[2264] Largest 3-Same-Digit Number in String.py
@@ -1,6 +1,21 @@
 """
 Accepted
 2264 [Easy]
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Largest 3-Same-Digit Number in String.
+Memory Usage: 17.84 MB, less than 39.13% of Java online submissions for Largest 3-Same-Digit Number in String.
+"""
+class Solution:
+    def largestGoodInteger(self, num: str) -> str:
+        numStr = str(num)
+        res = ""
+        for i in range(9, -1, -1):
+            if str(i) * 3 in numStr:
+                return str(i) * 3
+        return res
+
+
+
+"""
 Runtime: 38 ms, faster than 79.71% of Java online submissions for Largest 3-Same-Digit Number in String.
 Memory Usage: 16.25 MB, less than 59.27% of Java online submissions for Largest 3-Same-Digit Number in String.
 """
