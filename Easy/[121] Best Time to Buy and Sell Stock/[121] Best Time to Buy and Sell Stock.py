7f45755e791b916c094d9d8d76f23cf78ae6626d
Samarth Jain <jsamarth84@gmail.com>
2025-10-06 14:39:08 +0530

    LeetCode-Solutions

--- Easy/[121] Best Time to Buy and Sell Stock/[121] Best Time to Buy and Sell Stock.py
+++ Easy/[121] Best Time to Buy and Sell Stock/[121] Best Time to Buy and Sell Stock.py
@@ -4,6 +4,45 @@ Accepted
 Runtime: 863 ms, faster than 56.38% of Python3 online submissions for Best Time to Buy and Sell Stock.
 Memory Usage: 27.18 MB, less than 93.66% of Python3 online submissions for Best Time to Buy and Sell Stock.
 """
+# Greedy solution
+# TC: O(n), SC: O(1)
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minPrice = float("inf")
+        maxProfit = 0
+        for price in prices:
+            minPrice = min(minPrice, price)
+            maxProfit = max(maxProfit, price - minPrice)
+        return maxProfit
+
+
+
+"""
+Runtime: 189 ms, faster than 5.13% of Python3 online submissions for Best Time to Buy and Sell Stock.
+Memory Usage: 37.19 MB, less than 8.25% of Python3 online submissions for Best Time to Buy and Sell Stock.
+"""
+# HashMap solution
+# TC: O(n), SC: O(n)
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        curMaxPrice = prices[-1]
+        max_future_price_at_i = {}
+        for i in reversed(range(len(prices))):
+            curMaxPrice = max(curMaxPrice, prices[i])
+            max_future_price_at_i[i] = curMaxPrice
+
+        maxProfit = 0
+        for i in range(len(prices)):
+            maxProfit = max(maxProfit, max_future_price_at_i[i] - prices[i])
+        return max(maxProfit, 0)
+
+
+
+"""
+Runtime: 863 ms, faster than 56.38% of Python3 online submissions for Best Time to Buy and Sell Stock.
+Memory Usage: 27.18 MB, less than 93.66% of Python3 online submissions for Best Time to Buy and Sell Stock.
+"""
+# TC: O(n), SC: O(1)
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         left, right = 0, 1
