7ba2c0edb6612f91a89e6bc27d4664b35190a3cc
Samarth Jain <jsamarth84@gmail.com>
2025-10-25 19:37:44 +0530

    LeetCode-Solutions

--- Easy/[1716] Calculate Money in Leetcode Bank/[1716] Calculate Money in Leetcode Bank.py
+++ Easy/[1716] Calculate Money in Leetcode Bank/[1716] Calculate Money in Leetcode Bank.py
@@ -4,6 +4,8 @@ Accepted
 Runtime: 33 ms, faster than 91.19% of Python3 online submissions for Calculate Money in Leetcode Bank.
 Memory Usage: 16.36 MB, less than 7.77% of Python3 online submissions for Calculate Money in Leetcode Bank.
 """
+# Math approach
+# TC: O(1), SC: O(1)
 class Solution:
     def totalMoney(self, n: int) -> int:
         weeks = n // 7
@@ -15,3 +17,27 @@ class Solution:
             savings += weeks + 1 + i
 
         return savings
+
+
+
+"""
+Runtime: 0 ms, faster than 100.00% of Python3 online submissions for Calculate Money in Leetcode Bank.
+Memory Usage: 17.80 MB, less than 60.62% of Python3 online submissions for Calculate Money in Leetcode Bank.
+"""
+# Simulation approach
+# TC: O(n), SC: O(1)
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        res = 0
+
+        full_weeks = (n // 7)
+        res = full_weeks * (((7 + 1) * 7) // 2)
+
+        for i in range(1, full_weeks):
+            res += i * 7
+        
+        n = n % 7
+        for i in range(1, n + 1):
+            res += i + full_weeks
+
+        return res
