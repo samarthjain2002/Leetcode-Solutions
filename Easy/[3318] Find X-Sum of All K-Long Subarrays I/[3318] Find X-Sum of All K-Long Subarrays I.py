c59ab4b43a0cb050ff2f6e4f0b3366a08a4be9d0
Samarth Jain <jsamarth84@gmail.com>
2025-11-04 21:40:16 +0530

    LeetCode-Solutions

--- Easy/[3318] Find X-Sum of All K-Long Subarrays I/[3318] Find X-Sum of All K-Long Subarrays I.py
+++ Easy/[3318] Find X-Sum of All K-Long Subarrays I/[3318] Find X-Sum of All K-Long Subarrays I.py
@@ -4,6 +4,7 @@ Accepted
 Runtime: 21 ms, faster than 81.56% of Python3 online submissions for Find X-Sum of All K-Long Subarrays I.
 Memory Usage: 18.00 MB, less than 38.70% of Python3 online submissions for Find X-Sum of All K-Long Subarrays I.
 """
+# TC: O((n+k+1)klogk), SC: O(n+k)
 class Solution:
     def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
         res = []
