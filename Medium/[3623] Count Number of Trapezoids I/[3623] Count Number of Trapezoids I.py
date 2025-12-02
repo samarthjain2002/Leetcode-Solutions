59e852965166181d43c0b417eb7f0ce519a08332
Samarth Jain <jsamarth84@gmail.com>
2025-12-02 16:14:15 +0530

    LeetCode-Solutions

---
+++ Medium/[3623] Count Number of Trapezoids I/[3623] Count Number of Trapezoids I.py
@@ -0,0 +1,24 @@
+"""
+Accepted
+3623 [Medium]
+Runtime: 103 ms, faster than 12.09% of Python3 online submissions for Count Number of Trapezoids I.
+Memory Usage: 63.73 MB, less than 26.19% of Python3 online submissions for Power Grid Maintenance.
+"""
+class Solution:
+    def countTrapezoids(self, points: List[List[int]]) -> int:
+        MOD = 10**9 + 7
+
+        lines = defaultdict(int)
+        for x, y in points:
+            lines[y] += 1
+
+        total = 0
+        for line, points in lines.items():
+            total += (points * (points - 1)) // 2
+
+        res = 0
+        for line, points in lines.items():
+            total -= (points * (points - 1)) // 2
+            res += ((points * (points - 1)) // 2) * total
+            res %= MOD
+        return res
