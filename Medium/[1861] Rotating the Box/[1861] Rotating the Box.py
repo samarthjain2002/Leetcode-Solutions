fc3a07c5a2ab94b19ba3faab84c7c4344f0b6c36
Samarth Jain <jsamarth84@gmail.com>
2026-05-07 01:28:05 +0530

    LeetCode-Solutions

--- Medium/[1861] Rotating the Box/[1861] Rotating the Box.py
+++ Medium/[1861] Rotating the Box/[1861] Rotating the Box.py
@@ -1,6 +1,35 @@
 """
 Accepted
 1861 [Medium]
+Runtime: 143 ms, faster than 41.18% of Python3 online submissions for Rotating the Box.
+Memory Usage: 52.05 MB, less than 91.48% of Python3 online submissions for Rotating the Box.
+"""
+class Solution:
+    def rotateTheBox(self, boxGrid: List[List[str]]) -> List[List[str]]:
+        # Move right
+        for i in range(len(boxGrid)):
+            right = len(boxGrid[0]) - 1
+            for left in range(len(boxGrid[0]) - 1, -1, -1):
+                if boxGrid[i][left] == '.':
+                    continue
+                elif boxGrid[i][left] == '*':
+                    right = left - 1
+                elif boxGrid[i][left] == '#':
+                    boxGrid[i][left] = '.'
+                    boxGrid[i][right] = '#'
+                    right -= 1
+
+        # Rotate the box
+        newGrid = [['.'] * len(boxGrid) for _ in range(len(boxGrid[0]))]
+        for i in range(len(boxGrid)):
+            for j in range(len(boxGrid[0])):
+                newGrid[j][len(boxGrid) - i - 1] = boxGrid[i][j]
+
+        return newGrid
+
+
+
+"""
 Runtime: 1906 ms, faster than 44.09% of Python3 online submissions for Rotating the Box.
 Memory Usage: 30.44 MB, less than 5.11% of Python3 online submissions for Rotating the Box.
 """
