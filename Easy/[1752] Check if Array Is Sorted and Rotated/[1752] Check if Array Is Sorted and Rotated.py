7f9de420d18e197b73a35cb107e6159b095507ac
Samarth Jain <jsamarth84@gmail.com>
2026-05-23 22:23:27 +0530

    LeetCode-Solutions

--- Easy/[1752] Check if Array Is Sorted and Rotated/[1752] Check if Array Is Sorted and Rotated.py
+++ Easy/[1752] Check if Array Is Sorted and Rotated/[1752] Check if Array Is Sorted and Rotated.py
@@ -1,6 +1,24 @@
 """
 Accepted
 1752 [Easy]
+Runtime: 0 ms, faster than 100.00% of Python online submissions for Check if Array Is Sorted and Rotated.
+Memory Usage: 19.37 MB, less than 16.10% of Python online submissions for Check if Array Is Sorted and Rotated.
+"""
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        count = 0
+        n = len(nums)
+
+        for i in range(n):
+            if nums[i] > nums[(i + 1) % n]:
+                count += 1
+            if count > 1:
+                return False
+        return True
+
+
+
+"""
 Runtime: 38 ms, faster than 57.97% of Python online submissions for Check if Array Is Sorted and Rotated.
 Memory Usage: 17.64 MB, less than 70.45% of Python online submissions for Check if Array Is Sorted and Rotated.
 """
