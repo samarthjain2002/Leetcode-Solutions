19a62d7e54dfda569f8c6362ebb635bc2ecb18ac
Samarth Jain <jsamarth84@gmail.com>
2026-06-19 16:59:50 +0530

    LeetCode-Solutions

--- Easy/[1732] Find the Highest Altitude/[1732] Find the Highest Altitude.py
+++ Easy/[1732] Find the Highest Altitude/[1732] Find the Highest Altitude.py
@@ -1,14 +1,14 @@
 """
 Accepted
 1732 [Easy]
-Runtime: 38 ms, faster than 57.97% of Python online submissions for Find the Highest Altitude.
-Memory Usage: 16.49 MB, less than 90.91% of Python online submissions for Find the Highest Altitude.
+Runtime: 0 ms, faster than 100.00% of Python online submissions for Find the Highest Altitude.
+Memory Usage: 19.20 MB, less than 85.71% of Python online submissions for Find the Highest Altitude.
 """
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        N = len(gain)
-        prefixSum = 0
-        for i in range(N):
-            prefixSum += gain[i]
-            gain[i] = prefixSum
-        return max(0, max(gain))
+        altitude = 0
+        res = 0
+        for g in gain:
+            altitude += g
+            res = max(res, altitude)
+        return res
