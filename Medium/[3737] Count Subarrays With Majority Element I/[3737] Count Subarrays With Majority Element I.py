89432da153dcba37ba2b893678afcd9210a994e6
Samarth Jain <jsamarth84@gmail.com>
2026-06-25 11:13:31 +0530

    LeetCode-Solutions

---
+++ Medium/[3737] Count Subarrays With Majority Element I/[3737] Count Subarrays With Majority Element I.py
@@ -0,0 +1,19 @@
+"""
+Accepted
+3737 [Medium]
+Runtime: 104 ms, faster than 66.18% of Python3 online submissions for Count Subarrays With Majority Element I.
+Memory Usage: 31.07 MB less than 57.19% of Python3 online submissions for Count Subarrays With Majority Element I.
+"""
+# Brute-Force Solution
+# TC: O(n^2), SC: O(1)
+class Solution:
+    def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
+        res = 0
+        for i in range(len(nums)):
+            cur = 0
+            for j in range(i, len(nums)):
+                if nums[j] == target:
+                    cur += 1
+                if 2 * cur > j - i + 1:
+                    res += 1
+        return res
